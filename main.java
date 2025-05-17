6c2b3e6415b252d8266ca3d32c4a03600d19d438
loveAI0 <140394889+loveAI0@users.noreply.github.com>
2025-05-17 10:36:49 +0000

    Pricing of seats are done according to seat numbers.

--- main.java
+++ main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class main {
    public static void main(String[] args){
         for(int i = 0; i < 9; i++ ){
@@ -10,5 +11,40 @@ public class main {
             }
             else System.out.println((i - 1) + " S S S S S S S S");
         }
-   } 
+        // input total row and seats per row
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the number of rows:");
+        int row = input.nextInt();
+        System.out.println("Enter the number of seats in each row:");
+        int seat = input.nextInt();
+        main.totalIncome(row, seat);
+
+   }
+   static void totalIncome(int row, int seat){
+        double total = 0;
+        double ticketPrice = 0;
+        double totalSeat = row * seat;
+        if(totalSeat <= 60){
+            ticketPrice = 10;
+            total = row * seat * ticketPrice;
+        }
+        else{
+            if(row % 2 != 0){
+                 int frontRow = row / 2;
+                 double ticketPrice_Front = 10;
+                 int backRow = row - frontRow ;
+                 double ticketPrice_back = 8;
+                 total = seat * frontRow * ticketPrice_Front + seat * backRow * ticketPrice_back;
+            }
+            else{
+                int frontRow = row / 2;
+                double ticketPrice_Front = 10;
+                int backRow = row - frontRow ;
+                double ticketPrice_back = 8;
+                total =  seat * frontRow * ticketPrice_Front + seat * backRow * ticketPrice_back;
+            }
+        }
+        System.out.println("Total income:"+"\n"+"$" + Math.round(total));
+   }
 }
